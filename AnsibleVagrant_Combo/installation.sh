232ba66ea8394f2605eca65844c1f736bde9326b
Quchen FU <hbwhfqc@gmail.com>
2020-10-09 14:04:24 -0500

    update

---
+++ AnsibleVagrant_Combo/installation.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget http://mirrors.advancedhosters.com/apache/kafka/2.6.0/kafka_2.13-2.6.0.tgz
+sudo apt-get install python3-pip
+sudo apt-get install default-jdk
+sudo apt-get install net-tools
