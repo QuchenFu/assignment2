8ed928ca7df9869eff18f451280fb5b064ea6434
Quchen FU <hbwhfqc@gmail.com>
2020-10-08 23:03:55 -0500

    update

--- AnsibleVagrant_Combo/bootstrap.sh
+++ AnsibleVagrant_Combo/bootstrap.sh
@@ -6,3 +6,11 @@ mkdir -p ~/.ansible
 mkdir -p ~/.config
 mkdir -p ~/.config/openstack
 
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo python3 -m pip install --upgrade pip setuptools wheel
+sudo -H pip3 install --upgrade ansible
+sudo -H pip3 install --ignore-installed PyYAML
+sudo python3 -m pip install --upgrade openstacksdk
+ansible-galaxy collection install openstack.cloud
+
